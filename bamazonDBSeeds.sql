a0f72c33cde7a41e9d08968228ac4ebec2e07b47
landon <landon.seebart@gmail.com>
2018-05-05 08:30:44 -0500

    sql db created, seeded and connected

---
+++ bamazonDBSeeds.sql
@@ -0,0 +1,25 @@
+USE bamazonDB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coloring book", "Books", 10.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pizza", "Grocery", 5.00, 1000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Watch", "Fashion", 60.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Man Pants", "Fashion", 40.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Lady Pants", "Fashion", 40.00, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Car", "Auto", 15,000.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Lake Cabin", "Real Estate", 300,000.00, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Oreo Cookies", "Grocery", 3.00, 500);

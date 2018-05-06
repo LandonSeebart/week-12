ee5e205c3b191951cda89b2436da07c12fc18766
landon <landon.seebart@gmail.com>
2018-05-06 11:52:05 -0500

    as good as the sql stuff will get for now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,33 +13,60 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  getUserItemFrom(products);
-  connection.end();
+  getUserSelectionFrom(products)
 });
 
-function getUserItemFrom(list) {
+function getUserSelectionFrom(list) {
 
   choiceArray = productInfoArrBuilder(list);
 
   inquirer.prompt([
     {
     type: "list",
-    name: "userItem",
+    name: "item",
     message: "What would you like to buy?",
     choices: choiceArray
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "How many?"
     }
   ]).then(answers => {
-    console.log(answers.userItem)
+   let userSelection = userSelectionCleaner(answers.item)
+   console.log(userSelection);
+   let stockAvailible = getProductQuantity(userSelection)
+
+   if (stockAvailible - answers.quantity < 1) {
+    console.log("Insufficient quantity!");
+    } else {
+      console.log("done!");
+    }
   });
 }
 
 function productInfoArrBuilder(arr) {
 
   let productInfo = []
-
   for (let i = 0; i < arr.length; i++) {
     productInfo.push(`${arr[i].product_name} : $${arr[i].price}`);
   }
 
   return (productInfo)
+}
+
+function userSelectionCleaner(arg) {
+
+  let userItemArr = arg.split(':')
+  let userItemName = userItemArr[0]
+  return(userItemName);
+
+}
+
+function getProductQuantity(productName) {
+  const query = `SELECT stock_quantity FROM products WHERE ?`;
+    connection.query(query, { product_name: productName  }, function(err, res) {
+        if (err) throw err;
+        console.log(res[0].stock_quantity)
+    });
 }
--- test.js
+++ test.js
@@ -13,6 +13,25 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  
+  let item = "Coloring book"
+  let column = "stock_quantity"
+  getProductQuantity(column, item);
   connection.end();
 });
+
+function getProductQuantity(column, productName) {
+  const query = `SELECT ${column} FROM products WHERE ?`;
+    connection.query(query, { product_name: productName  }, function(err, res) {
+        if (err) throw err;
+        console.log(res[0].stock_quantity)
+    });
+}
+
+// function setProductQuantity(column, product_name, data) {
+//   if (err) throw err;
+//   const query = "UPDATE products SET stock_quantity = '100' WHERE ?";
+//   connection.query(query,{product_name: productName } function (err, res) {
+//     if (err) throw err;
+//     console.log(result.affectedRows + " record(s) updated");
+//   });
+// });
